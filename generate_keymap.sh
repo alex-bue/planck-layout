b57fefdd87ac0722ae716b4538744f6c27bf7f4f
Alexander Büscher <alibue@hotmail.de>
2024-05-19 20:00:19 +0200

    Add generate keymap layout script with keymap-drawer

---
+++ generate_keymap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define variables
+KEYBOARD="planck/ez/glow"
+KEYMAP="alex-bue"
+KEYMAP_C="keymap/keymap.c"
+
+CONFIG_FILE="keymap_drawer.config.yaml"
+LAYERS="BASE RAISE LOWER NAV ADJUST"
+OUTPUT_FILE="assets/layout.svg"
+
+# Convert keymap.c to JSON and pipe it directly to the next command
+./qmk.sh c2json "$KEYMAP_C" -kb "$KEYBOARD" -km "$KEYMAP" | \
+poetry run keymap -c "$CONFIG_FILE" parse -l $LAYERS -c 12 -q - > keymap.yaml
+poetry run keymap -c "$CONFIG_FILE" draw -o "$OUTPUT_FILE" keymap.yaml
