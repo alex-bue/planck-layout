ad2ccbfe94e55ae3a9559a57c6e6f3b1cdc81b02
Alexander Büscher <alibue@hotmail.de>
2024-05-19 11:02:53 +0200

    Add workflow to automatically draw layout

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,          KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,     KC_Y,     KC_U,   KC_I,     KC_O,     KC_P,     KC_BSPC,
     LCTL_T(KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,   KC_G,     KC_H,     KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,         KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,     KC_N,     KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  SC_SENT,
-    NAV,             KC_LCTL,  KC_LGUI,  KC_LALT,  LOWER,  NAV_SPC,  NAV_SPC,  RAISE,  KC_RALT,  KC_RGUI,  KC_RCTL,  NAV
+    MO(_NAVIGATION), KC_LCTL,  KC_LGUI,  KC_LALT,  LT(_LOWER, KC_BSPC),  LT(_NAVIGATION, KC_SPC),  LT(_NAVIGATION, KC_SPC),  LT(_RAISE, KC_ENTER),  KC_RALT,  KC_RGUI,  KC_RCTL,  MO(_NAVIGATION)
 ),
 
 [_LOWER] = LAYOUT_ortho_4x12(
